b91406232c5430cb5ca747c68bc6130581c59a1e
unknown <pjhq2@naver.com>
2021-11-16 15:55:33 +0900

    day1

--- community/urls.py
+++ community/urls.py
@@ -1,5 +1,15 @@
+from django.conf.urls import url
 from django.urls import path
+from . import views
+
+app_name = 'community'
 
 urlpatterns = [
-    
+    path('', views.index, name='index'),
+    path('create/', views.create_review, name='create'),
+    path('<int:review_pk>', views.detail_review, name='detail'),
+    path('<int:review_pk>/update/', views.update_review, name='update'),
+    path('<int:review_pk>/delete/', views.delete_review, name='delete'),
+    path('<int:review_pk>/comments/create/', views.create_comment, name='create_comment'),
+    path('<int:review_pk>/comments/<int:comment_pk>/delete/', views.delete_comment, name='delete_comment'),
 ]
